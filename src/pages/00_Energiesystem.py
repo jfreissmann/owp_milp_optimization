0717f377fa83742bc76e45b8f2c7f6af3af21c0c
maltefritz <malte.fritz@web.de>
2024-04-16 14:45:36 +0200

    Add first attempt for energy system tab.

--- src/pages/00_Energiesystem.py
+++ src/pages/00_Energiesystem.py
@@ -1,17 +1,34 @@
+import os
+
+import darkdetect
 import streamlit as st
 
-tab1, tab2 = st.tabs(["Tab 1", "Tab 2"])
+is_dark = darkdetect.isDark()
+logo = os.path.join(__file__, '..', '..', 'img', 'Logo_ZNES_mitUnisV2.svg')
+
+tab1, tab2, tab3, tab4 = st.tabs(
+    ['System', 'Anlagen','Wärmelast', 'Energiepreise']
+    )
 
 with tab1:
-    col1, col2 = st.columns(2)
-    with col1:
-        st.selectbox("City", ["City1", "City2"])
-    with col2:
-        st.selectbox("District", ["District1", "District2"])
+    st.header('Wärmeversorgungssystem')
+
+    units = st.multiselect(
+        'Wähle die Wärmeversorgungsanlagen aus, die im System verwendet werden können:',
+        ['Wärmepumpe', 'GuD', 'BHKW', 'Solarthermie', 'SLK', 'TES'],
+        placeholder='Wärmeversorgungsanlagen'
+        )
+    print(units)
+
+    for i in units:
+        st.image(logo)
+        st.write(i)
 
 with tab2:
-    col1, col2 = st.columns(2)
-    with col1:
-        st.selectbox("City", ["AnotherCity1", "AnotherCity2"])
-    with col2:
-        st.selectbox("District", ["AnotherDistrict1", "AnotherDistrict2"])
+    st.write('Hier die ausgewählten Anlagen parametrisieren')
+
+with tab3:
+    st.write('Hier die Wärmelast und den Zeitraum auswählen')
+
+with tab4:
+    st.write('Hier alle Energiepreise aufzuzeigen')
