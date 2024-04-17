8695b330ea38d6a92357d903b8b19511b8dac4be
jfreissmann <j.freissmann@web.de>
2024-04-17 12:40:55 +0200

    Rename images, export them to png and implement handling in app.

--- src/pages/00_Energiesystem.py
+++ src/pages/00_Energiesystem.py
@@ -13,16 +13,24 @@ tab1, tab2, tab3, tab4 = st.tabs(
 with tab1:
     st.header('Wärmeversorgungssystem')
 
+    pngnames = {
+        'Wärmepumpe': 'hp',
+        'Gas- und Dampfkratwerk': 'ccet',
+        'Blockheizkraftwerk': 'ice',
+        'Solarthermie': 'sol',
+        'Spitzenlastkessel': 'plb',
+        'Wärmespeicher': 'tes',
+    }
+
     units = st.multiselect(
         'Wähle die Wärmeversorgungsanlagen aus, die im System verwendet werden können:',
-        ['Wärmepumpe', 'GuD', 'BHKW', 'Solarthermie', 'SLK', 'TES'],
+        list(pngnames.keys()),
         placeholder='Wärmeversorgungsanlagen'
         )
-    print(units)
 
-    st.image(f'{topopath}header.svg')
-    for i in units:
-        st.image(f'{topopath+i}.svg')
+    st.image(f'{topopath}header.png', width=500)
+    for unit in units:
+        st.image(f'{topopath+pngnames[unit]}.png', width=500)
 
 with tab2:
     st.write('Hier die ausgewählten Anlagen parametrisieren')
