e38f2f6b7d7dfe098d3c02e19e0265de6ac8a069
Jonas Freißmann <j.freissmann@web.de>
2024-04-29 15:57:31 +0200

    Add landing page to Home.py

--- src/Home.py
+++ src/Home.py
@@ -21,3 +21,86 @@ with st.sidebar:
         logo = os.path.join(__file__, '..', 'img', 'Logo_ZNES_mitUnisV2.svg')
     st.image(logo, use_column_width=True)
 
+# %% Main Window
+st.write(
+    """
+    Willkommen in dem Optimierungsdashboard des Projektes "Offene Wärmespeicherplanung"
+    (OWP) der Initiative Inno!Nord. 
+
+    Mit diesem Dashboard lassen sich multivalente Wärmeversorgungssysteme und
+    die darin eingesetzen Versorgungsanlagen simulieren. Dazu kann aus einer
+    Reihe von Anlagentypen gewählt werden, die Ihr individuelles Wärmesystem
+    bilden. Diese Anlagen werden anschließend parametrisiert, die
+    energiewirtschaftlichen und -politischen Rahmenbedingungen festgelegt und
+    eine kombinierte Auslegungs- und Einsatzoptimierung durchgeführt. Dazu wird
+    die Methode der gemischt ganzzahlig linearen Optimierung (kurz MILP, aus dem
+    Englischen: Mixed Integer Linear Programming) verwendet.
+
+    ### Key Features
+
+    - Kombiniert Auslegungs- und Einsatzoptimierung basierend auf [oemof-solph](https://github.com/oemof/oemof-solph)
+    - Parametrisierung and Ergebnisvisualisierung mithilfe eines [Streamlit](https://github.com/streamlit/streamlit) Dashboards
+    - Breite Auswahl typischer Wärmeversorgungsanlagen
+    - Umfangreiche Datenbank von Lastdaten, Preiszeitreihen und Emissionsfaktoren
+    """
+    )
+
+st.markdown("""---""")
+
+with st.expander('Verwendete Software'):
+    st.info(
+        """
+        #### Verwendete Software:
+
+        Zur Modellerstellung und Simulationen wird die
+        Open Source Software oemof-solph verwendet. Des Weiteren werden
+        eine Reihe weiterer Pythonpakete zur Datenverarbeitung,
+        -aufbereitung und -visualisierung genutzt.
+
+        ---
+
+        #### oemof-solph:
+
+        Oemof-solpf als Teil des Open Energy Modelling Framework ist ein
+        leistungsfähiges Simulationswerkzeug für Energiesysteme. Mit dem Paket
+        ist es möglich, den Anlageneinsatz zu optimieren und die Kapazität dieser
+        auszulegen. Die komponentenbasierte Struktur in
+        Kombination mit den generischen Anlagenklassen bieten eine sehr hohe
+        Flexibilität hinsichtlich der Systemtopologie und der
+        Parametrisierung. Weitere Informationen zu oemof-solph sind in dessen
+        [Onlinedokumentation](https://oemof-solph.readthedocs.io) in
+        englischer Sprache zu finden.
+
+        #### Weitere Pakete:
+
+        - [Streamlit](https://docs.streamlit.io) (Graphische Oberfläche)
+        - [NumPy](https://numpy.org) (Datenverarbeitung)
+        - [pandas](https://pandas.pydata.org) (Datenverarbeitung)
+        - [Matplotlib](https://matplotlib.org) (Datenvisualisierung)
+        """
+        )
+
+with st.expander('Disclaimer'):
+    st.warning(
+        """
+        #### Simulationsergebnisse:
+
+        Numerische Simulationen sind Berechnungen mittels geeigneter
+        Iterationsverfahren in Bezug auf die vorgegebenen und gesetzten
+        Randbedingungen und Parameter. Eine Berücksichtigung aller
+        möglichen Einflüsse ist in Einzelfällen nicht möglich, so dass
+        Abweichungen zu Erfahrungswerten aus Praxisanwendungen
+        entstehen können und bei der Bewertung berücksichtigt werden
+        müssen. Entsprechend sind alle Angaben und Ergebnisse ohne Gewähr.
+        """
+        )
+
+with st.expander('Copyright'):
+    licpath = os.path.abspath(os.path.join(
+        os.path.dirname(__file__), '..', 'LICENSE'
+        ))
+    with open(licpath, 'r', encoding='utf-8') as file:
+        lictext = file.read()
+
+    st.success('#### Softwarelizenz\n' + lictext.replace('(c)', '©'))
+
