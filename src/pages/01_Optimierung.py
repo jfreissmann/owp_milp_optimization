771a799bdacb6fe3cc5e19187b21c36d32a813a8
Jonas Freißmann <j.freissmann@web.de>
2024-06-07 12:55:14 +0200

    Add abbility to download all relevant data in zip archive.

--- src/pages/01_Optimierung.py
+++ src/pages/01_Optimierung.py
@@ -1,11 +1,52 @@
 import json
 import os
+import shutil
 
 import pandas as pd
 import streamlit as st
 from streamlit import session_state as ss
 
 from model import EnergySystem
+
+
+@st.experimental_dialog('Energiesystem lokal speichern')
+def download_energy_system():
+    """Temporarely save data and zip it, the let user download zip archive."""
+    with st.spinner('Daten werden verarbeitet...'):
+        tmppath = os.path.abspath(
+            os.path.join(
+                os.path.dirname(__file__), '..', '_tmp'
+            )
+        )
+        if not os.path.exists(tmppath):
+            os.mkdir(tmppath)
+
+        zippath = os.path.join(tmppath, 'energysystem')
+        if not os.path.exists(zippath):
+            os.mkdir(zippath)
+
+        tspath = os.path.join(zippath, 'data_input.csv')
+        ss.data.to_csv(tspath, sep=';')
+
+        optpath = os.path.join(zippath, 'param_opt.json')
+        with open(optpath, 'w', encoding='utf-8') as file:
+            json.dump(ss.param_opt, file, indent=4, sort_keys=True)
+
+        unitpath = os.path.join(zippath, 'param_units.json')
+        with open(unitpath, 'w', encoding='utf-8') as file:
+            json.dump(ss.param_units, file, indent=4, sort_keys=True)
+
+        shutil.make_archive(zippath, 'zip', zippath)
+
+    with open(f'{zippath}.zip', 'rb') as file:
+        btn = st.download_button(
+            label='Speichere dein Energiesystem',
+            data=file,
+            file_name='Energiesystem',
+            mime='application/zip'
+        )
+
+    shutil.rmtree(tmppath)
 
 shortnames = {
     'Wärmepumpe': 'hp',
@@ -120,7 +161,8 @@ col_over.dataframe(param_overview, use_container_width=True)
 
 # %% MARK: Save Data
 # _, col_save, col_reset, _ = st.columns([1.05, 1, 1, 2], gap='large')
-col_save, col_reset = col_over.columns([1, 1], gap='large')
+# col_save, col_reset = col_over.columns([1, 1], gap='large')
+col_save, col_dl_es, col_reset = col_over.columns([1, 1, 1], gap='large')
 savepath = os.path.abspath(
     os.path.join(os.path.dirname(__file__), '..', 'save')
     )
@@ -145,6 +187,14 @@ if download:
     unitpath = os.path.join(savepath, 'param_units.json')
     with open(unitpath, 'w', encoding='utf-8') as file:
         json.dump(ss.param_units, file, indent=4, sort_keys=True)
+
+download_es_btn = col_dl_es.button(
+    label='📝 Energiesystem speichern',
+    key='download_es_button_overview',
+    use_container_width=True
+    )
+if download_es_btn:
+    download_energy_system()
 
 reset_es = col_reset.button(
     label='📝 Neues Energiesystem konfigurieren',
